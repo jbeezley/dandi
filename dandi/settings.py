3ffd8fa6b9523dbae4cec8245623bf6ef4bb307f
Jonathan Beezley <jonathan.beezley@kitware.com>
2019-11-03 14:13:47 -0500

    Add debug toolbar

--- dandi/settings.py
+++ dandi/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = '0zns04s$=)rj-cewdv5zc30-e2$+9*^xp$$$np&3&juhni7zy@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['garant', 'localhost', '127.0.0.1']
 
+INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django_filters',
     'djaunty',
     'rest_framework',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'dandi.urls'
--- dandi/urls.py
+++ dandi/urls.py
@@ -13,16 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
+
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
 
 from djaunty.views import DatasetViewSet
+
+from rest_framework import routers
+
 
 router = routers.DefaultRouter()
 router.register('datasets', DatasetViewSet)
 
 urlpatterns = [
+    path('__debug__', include(debug_toolbar.urls)),
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
